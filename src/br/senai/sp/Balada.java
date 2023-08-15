0160119fa9b373a0e6690ab49582331d55d727af
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-08-15 11:16:22 -0300

    balada

---
+++ src/br/senai/sp/Balada.java
@@ -0,0 +1,54 @@
+package br.senai.sp;
+
+import java.util.Scanner;
+
+public class Balada {
+
+    public static void main(String[] args) {
+
+        String nome, nomeAcompanhante;
+        long cpf, idadeAcompanhante;
+        int idade,idaeAcompanhate=0;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Seja bem vindo(a) a balada FunnyDrink");
+        System.out.print("Digite seu nome: ");
+        nome = teclado.nextLine();
+        System.out.print("Digite seu CPF: ");
+        cpf = teclado.nextLong();
+        System.out.print("Digite seu RG: ");
+        System.out.print("Digite sua idade: ");
+        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/--//");
+
+
+        /** coletar dados */
+
+        System.out.println("/-/-/-/-/-/-/-/-/-/-/");
+        System.out.println("Bem vido a nossa Balada!!");
+        System.out.println("Qual o nome do Acompanhante:");
+        nomeAcompanhante=teclado.next();
+         System.out.println("Ola",+"");
+        idade = teclado.nextInt();{
+         } else {
+             System.out.print("pessoa digite um numero inteiro");
+             teclado.next();
+             else if (idade >= 18 && idade % 2 ==)
+         }
+
+            if ( idade >=18 && idade %2 == 0 &&idadeAcompanhante >= 18 ) {
+                System.out.println("curtam Bastante a Nossa Balade!!!");
+                else {
+                    System.out.println("Vão Enbora");
+
+
+            }
+
+
+
+        }
+    }
+}
+
+
+
